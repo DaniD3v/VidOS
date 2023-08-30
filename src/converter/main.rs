93e61092b9586109ac4ad317f180237f1ed860ac
DaniD3v <sch220233@spengergasse.at>
2023-08-30 21:48:24 +0200

    added README.md

--- src/converter/main.rs
+++ src/converter/main.rs
@@ -1,8 +1,8 @@
-use sha2::{Digest, Sha256};
+use std::collections::hash_map::DefaultHasher;
 use std::ffi::OsString;
 
-use std::fs::{read_dir, DirEntry, File};
-use std::io;
+use std::fs::{read_dir, DirEntry};
+use std::hash::Hasher;
 use std::io::Error;
 
 fn main() {
@@ -14,20 +14,18 @@ fn get_unconverted_files() -> Result<Vec<DirEntry>, Error> {
         .map(|entry| entry.map(|x| x.file_name()))
         .collect::<Result<_, _>>()?;
 
-    Ok(read_dir("./videos/in")?
-        .map(|file| {
-            file.map(|file| {
-                let mut hasher = Sha256::new();
-                io::copy(&mut File::open(file.path()).ok()?, &mut hasher).ok()?;
-                let name = format!("{:?}.data", hasher.finalize());
+    Ok(read_dir("./videos/in")?.map(|file|  {
+        file.map(|file| {
+            let mut hasher = DefaultHasher::new();
+            let contents = std::fs::read_to_string(file.path()).ok()?;
+            hasher.write(contents.as_bytes());
+            let name = format!("{:x}.data", hasher.finish());
 
-                println!("{name}");
+            println!("{name}");
 
-                (!out_names.contains(&OsString::from(name))).then_some(file)
-            })
-                .ok()
-                .flatten()
-        })
-        .collect::<Option<Vec<_>>>()
-        .unwrap_or_default())
+            let name = OsString::from(name);
+
+            (!out_names.contains(&name)).then_some(file)
+        }).ok().flatten()
+    }).collect::<Option<Vec<_>>>().unwrap_or_default())
 }
--- src/os/main.rs
+++ src/os/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! { loop {} }
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {};
 }
