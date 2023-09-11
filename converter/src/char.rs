4d4860d73e260c28bb352fe1d4bb9fac3ddf4242
DaniD3v <sch220233@spengergasse.at>
2023-09-11 11:50:58 +0200

    reorganized.
    This totally screwed up performance but whatever

--- converter/src/char.rs
+++ converter/src/char.rs
@@ -1,65 +1,84 @@
-use crate::constants::{BACKGROUND, CHARACTERS, CHAR_HEIGHT, CHAR_WIDTH, FOREGROUND};
-use image::{Rgb, RgbImage};
+use std::time::SystemTime;
 
-pub struct Char {
-    bitmap: &'static [u16; 9],
-    foreground_color: [u8; 3],
-    background_color: [u8; 3],
+use image::{GenericImageView, Rgb, RgbImage};
+use image::imageops::{FilterType, resize};
+
+use crate::constants::{BACKGROUND, CHAR_HEIGHT, CHAR_WIDTH, CODEPAGE_737, DOWNSCALE_HEIGHT, DOWNSCALE_WIDTH, FOREGROUND, POSSIBLE_CHARS};
+
+#[derive(Debug, Clone, Copy)]
+pub struct VGAChar {
+    char: u8,
+    foreground: u8,
+    background: u8,
 }
 
-impl Char {
-    pub fn new(char: usize, foreground: usize, background: usize) -> Self {
-        Char {
-            bitmap: &CHARACTERS[char],
-            foreground_color: FOREGROUND[foreground],
-            background_color: BACKGROUND[background],
+
+
+impl VGAChar {
+    pub fn new(char: u8, foreground: u8, background: u8) -> Self {
+        if foreground >= FOREGROUND.len() as u8 || background >= BACKGROUND.len() as u8 {
+            panic!("Invalid color");
+        }
+
+        Self {
+            char,
+            foreground,
+            background,
         }
     }
 
-    pub fn get_color(&self, x: u8, y: u8) -> Rgb<u8> {
-        match self.bitmap[x as usize] & (1 << y) != 0 {
-            true => Rgb::from(self.foreground_color),
-            false => Rgb::from(self.background_color),
+    pub fn generate_lookup_table() -> [(Self, RgbImage, RgbImage); POSSIBLE_CHARS] {
+        let now = SystemTime::now();
+        let mut table: Vec<(Self, RgbImage, RgbImage)> = Vec::with_capacity(POSSIBLE_CHARS);
+
+
+        for char in 0..=255 {
+            for foreground in 0..FOREGROUND.len() as u8 {
+                for background in 0..BACKGROUND.len() as u8 {
+                    let char = Self::new(char, foreground, background);
+                    let render = char.render();
+                    let downscale = resize(&render, DOWNSCALE_WIDTH, DOWNSCALE_HEIGHT, FilterType::Nearest);
+                    
+                    table.push((char, render, downscale))
+                }
+            }
         }
+
+        println!("Generating lookup table took {:?}", now.elapsed().unwrap());
+        table.try_into().unwrap()
     }
 
-    pub fn to_image(&self) -> RgbImage {
+    pub fn vga_format(&self) -> u16 {
+        self.char as u16 | ((self.foreground as u16) << 8) | ((self.background as u16) << 12)
+    }
+
+    pub fn lookup_index(&self) -> usize {
+        self.char as usize * FOREGROUND.len() * BACKGROUND.len() + self.foreground as usize * BACKGROUND.len() + self.background as usize
+    }
+
+    fn render(&self) -> RgbImage {
         let mut image = RgbImage::new(CHAR_WIDTH, CHAR_HEIGHT);
+        let bitmap = self.get_bitmap();
 
         for x in 0..CHAR_WIDTH {
             for y in 0..CHAR_HEIGHT {
-                image.put_pixel(x, y, self.get_color(x as u8, y as u8));
+                let color = match bitmap.get_pixel(x, y).0 {
+                    [170, 170, 170] => FOREGROUND[self.foreground as usize],
+                    [0, 0, 0] => BACKGROUND[self.background as usize],
+                    _ => panic!()
+                };
+
+                image.put_pixel(x, y, Rgb(color))
             }
         }
 
         image
     }
-}
 
-pub struct CharID {
-    id: u16,
-}
+    fn get_bitmap(&self) -> RgbImage {
+        let x = self.char as u32 % 32; // there's 32 chars each row
+        let y = self.char as u32 / 32;
 
-impl CharID {
-    pub fn new() -> Self {
-        CharID { id: 0 }
-    }
-}
-
-impl Iterator for CharID {
-    type Item = Char;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.id == 2u16.pow(15) {
-            return None;
-        }
-
-        let char = (self.id % 256) as usize;
-        let foreground = ((self.id / 256) % 16) as usize;
-        let background = ((self.id / 4096) % 8) as usize;
-
-        self.id += 1;
-
-        Some(Char::new(char, foreground, background))
+        CODEPAGE_737.view(x*CHAR_WIDTH, y*CHAR_HEIGHT, CHAR_WIDTH, CHAR_HEIGHT).to_image()
     }
 }
