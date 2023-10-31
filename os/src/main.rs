0bb13e7d14ecde286b09a706b32310055608ac2f
DaniD3v <sch220233@spengergasse.at>
2023-10-31 23:47:35 +0100

    Made OS display serialized images

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,10 +1,17 @@
 #![no_std]
 #![no_main]
 
+const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
+static IMAGE: &[u8; 4000] = include_bytes!("../../converter/examples/images/ser/incredible.bin");
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! { loop {} }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {};
+    for (i, &byte) in IMAGE.iter().enumerate() {
+        unsafe { *VGA_BUFFER.offset(i as isize) = byte; }
+    }
+
+    loop {}
 }
